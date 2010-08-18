a69d17003295af41a408d1eca6711a3f2cb2d10c
Birkir R Gudjonsson <birkir.gudjonsson@gmail.com>
2010-08-18 23:37:51 +0000

    Added config file which was ignored

---
+++ config/kogit.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	'git' => '/usr/bin/git',
+	'commit' => array(
+		'message' => array(
+			'maxlength' => 128
+		)
+	)
+);
